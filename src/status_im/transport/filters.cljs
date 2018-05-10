5cf1227bc5a99451b1167cd54727c0a93d5ec139
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-05-10 14:32:47 +0200

    formatting

--- src/status_im/transport/filters.cljs
+++ src/status_im/transport/filters.cljs
@@ -33,46 +33,46 @@
       (log/error "Could not create filter for" options))))
 
 (re-frame/reg-fx
-  :shh/add-filter
-  (fn [{:keys [web3 sym-key-id topic chat-id one-to-one]}]
-    (add-filter!
+ :shh/add-filter
+ (fn [{:keys [web3 sym-key-id topic chat-id one-to-one]}]
+   (add-filter!
+    web3
+    {:topics  [topic]
+     :event ::filter-added
+     :chat-id chat-id
+     :symKeyID sym-key-id}
+    (partial receive-message chat-id))
+   ;; We add a noop filter to avoid identification
+   (when one-to-one
+     (add-filter!
       web3
-      {:topics  [topic]
-       :event ::filter-added
+      {:topics [(utils/get-topic chat-id)]
+       :event  ::filter-added
        :chat-id chat-id
-       :symKeyID sym-key-id}
-      (partial receive-message chat-id))
-    ;; We add a noop filter to avoid identification
-    (when one-to-one
-      (add-filter!
-        web3
-        {:topics [(utils/get-topic chat-id)]
-         :event  ::filter-added
-         :chat-id chat-id
-         :minPow 1
-         :symKeyId sym-key-id}
-        (constantly nil)))))
+       :minPow 1
+       :symKeyId sym-key-id}
+      (constantly nil)))))
 
 (handlers/register-handler-db
-  ::filter-added
-  [re-frame/trim-v]
-  (fn [db [chat-id filter]]
-    (update-in db [:transport/chats chat-id :filters] conj filter)))
+ ::filter-added
+ [re-frame/trim-v]
+ (fn [db [chat-id filter]]
+   (update-in db [:transport/chats chat-id :filters] conj filter)))
 
 (re-frame/reg-fx
-  :shh/add-discovery-filter
-  (fn [{:keys [web3 private-key-id topic]}]
-    (add-filter! web3
-                 {:topics [topic]
-                  :event ::discovery-filter-added
-                  :privateKeyID private-key-id}
-                 (partial receive-message nil))))
+ :shh/add-discovery-filter
+ (fn [{:keys [web3 private-key-id topic]}]
+   (add-filter! web3
+                {:topics [topic]
+                 :event ::discovery-filter-added
+                 :privateKeyID private-key-id}
+                (partial receive-message nil))))
 
 (handlers/register-handler-db
-  ::discovery-filter-added
-  [re-frame/trim-v]
-  (fn [db [_ filter]]
-    (assoc db :transport/discovery-filter filter)))
+ ::discovery-filter-added
+ [re-frame/trim-v]
+ (fn [db [_ filter]]
+   (assoc db :transport/discovery-filter filter)))
 
 (re-frame/reg-fx
  :shh/remove-filter
